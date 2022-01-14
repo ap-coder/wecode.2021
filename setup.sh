bf52d4839bea9f61b7bd8caa57a58a34aa599a4e
Phillip Madsen <58571584+ap-coder@users.noreply.github.com>
2022-01-14 12:05:42 -0700

    docker stuff

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# run with "bash setup.sh" do not use sudo
 composer require laravel/sail --dev --ignore-platform-reqs
 composer install --ignore-platform-reqs
 php artisan sail:install --with="mariadb"
